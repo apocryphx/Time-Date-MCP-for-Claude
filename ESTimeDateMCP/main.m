c54763604e4f6a37190f5b4e61ef857a3d0f6970
Kolja Wawrowsky <3075215+apocryphx@users.noreply.github.com>
2026-04-13 15:50:33 -0700

    Initial Commit

---
+++ ESTimeDateMCP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ESTimeDateMCP
+//
+//  Created by Kolja Wawrowsky on 4/13/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return EXIT_SUCCESS;
+}
